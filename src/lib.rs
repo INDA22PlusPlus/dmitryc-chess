fb50e101cf801a16fe2898d72d381a1693a6d2e3
Dmitry Chirin <chirin.dima@gmail.com>
2022-09-16 00:13:23 +0200

    fix display for impl of CustomOption, add Hash, PartialEq and Eq derives to some enums and structs for future testing, add PieceTypes enum, add piece_type field to Piece, add comments, add functions to convert from PieceType enum value to notation string, rewrite related code and fixed endless bugs with implementation, need to separate structs and enums into separate files to test them more thoroughly

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,45 @@
+use std::collections::HashMap;
 use std::fmt;
 
 #[derive(Clone)]
 struct CustomOption<Piece>(Option<Piece>);
 
-impl<Piece> fmt::Display for CustomOption<Piece> {
+impl fmt::Display for CustomOption<Piece> {
     fn fmt(&self, formatter: &mut fmt::Formatter<>) -> fmt::Result {
-        match self.0 {
-            Some(ref piece) => write!(formatter, "P "),
+        match &self.0 {
+            Some(ref piece) => write!(formatter, "{}", piece.string_name),
             None => write!(formatter, "  "),
         }
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
 struct Size {
     w:u8,
     h:u8,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 struct Piece {
     color: Color,
+    piece_type: PieceTypes,
     string_name: String,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 enum Color{
     White,
     Black
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+enum PieceTypes{
+    King,
+    Rook,
+    Bishop,
+    Queen,
+    Knight,
+    Pawn,
 }
 
 struct ChessEngine{
@@ -40,11 +53,16 @@ impl ChessEngine {
         Self::create_engine_with_empty_board()
     }
 
+    // Non-readable code, needs refactoring later
     fn create_engine_with_white_board() -> ChessEngine {
         let size_temp = Size {w:8, h:8};
         ChessEngine {
             size: Size {w: *&size_temp.w, h:*&size_temp.h},
-            board: vec![vec![CustomOption(Some(Piece { color: Color::White, string_name: "P".to_string() })); *&size_temp.w as usize];
+            board: vec![vec![CustomOption(Some(Piece { 
+                color: Color::White,
+                piece_type: PieceTypes::King,
+                string_name: Self::enum_piece_to_notation(&PieceTypes::King)
+            })); *&size_temp.w as usize];
                         *&size_temp.h as usize],
         }
     }
@@ -90,6 +108,24 @@ impl ChessEngine {
             numbered_rank -= 1;
         });
         println!("  {}", "-".repeat((&self.size.w * 3 + 1) as usize));
+    }
+
+    fn get_piece_to_notation_hash() -> HashMap<PieceTypes, String> {
+        let mut map: HashMap<PieceTypes, String> = HashMap::new();
+        map.insert(PieceTypes::King, "K ".to_string());
+        map.insert(PieceTypes::Rook, "R ".to_string());
+        map.insert(PieceTypes::Bishop, "B ".to_string());
+        map.insert(PieceTypes::Queen, "Q ".to_string());
+        map.insert(PieceTypes::Knight, "Kn".to_string());
+        map.insert(PieceTypes::Pawn, "P ".to_string());
+
+        map
+    }
+
+    fn enum_piece_to_notation(piece: &PieceTypes) -> String {
+        (&Self::get_piece_to_notation_hash()
+            .get(&piece)
+            .unwrap()).to_string()
     }
 }
 
